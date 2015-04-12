4a750f953d45cd2b63329f820e53916bb7f70f88
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-12 17:47:17 +0200

    build jsoncpp in all configurations on local machine

--- main.bat
+++ main.bat
@@ -3,11 +3,14 @@ REM build dir will be used to downlaod && build dependencies
 
 mkdir build
 
-jsoncpp.bat || goto :error
-
+set PLATFORM=Win32 & set CONFIGURATION=Debug   & jsoncpp.bat || goto :error
+set PLATFORM=Win32 & set CONFIGURATION=Release & jsoncpp.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Debug   & jsoncpp.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Release & jsoncpp.bat || goto :error
 
 :goto :EOF
 
 :error
 echo Failed with error %errorlevel%
 exit /b %errorlevel%
+
