85953a07668a87b2fd2b87afa42384181de2e06f
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-13 00:28:19 +0200

    fixed order of instructions in main.bat

--- main.bat
+++ main.bat
@@ -3,8 +3,8 @@ set MSBuild="c:/Program Files (x86)/MSBuild/12.0/bin/msbuild.exe"
 
 REM create build, package && install directory
 if not exist build mkdir build
-if not exist install mkdir install
 if not exist package mkdir package
+if not exist install mkdir install
 
 set PLATFORM=Win32 & set CONFIGURATION=Debug   & call build_jsoncpp.bat || goto :error
 set PLATFORM=Win32 & set CONFIGURATION=Release & call build_jsoncpp.bat || goto :error
@@ -12,16 +12,16 @@ set PLATFORM=x64   & set CONFIGURATION=Debug   & call build_jsoncpp.bat || goto 
 set PLATFORM=x64   & set CONFIGURATION=Release & call build_jsoncpp.bat || goto :error
 call bundle_jsoncpp.bat || goto :error
 
-set PLATFORM=Win32 & set CONFIGURATION=Debug   & call build_curl.bat || goto :error
-set PLATFORM=Win32 & set CONFIGURATION=Release & call build_curl.bat || goto :error
-set PLATFORM=x64   & set CONFIGURATION=Debug   & call build_curl.bat || goto :error
-set PLATFORM=x64   & set CONFIGURATION=Release & call build_curl.bat || goto :error
-call bundle_microhttpd.bat || goto :error
-
 set PLATFORM=Win32 & set CONFIGURATION=Debug   & call build_microhttpd.bat || goto :error
 set PLATFORM=Win32 & set CONFIGURATION=Release & call build_microhttpd.bat || goto :error
 set PLATFORM=x64   & set CONFIGURATION=Debug   & call build_microhttpd.bat || goto :error
 set PLATFORM=x64   & set CONFIGURATION=Release & call build_microhttpd.bat || goto :error
+call bundle_microhttpd.bat || goto :error
+
+set PLATFORM=Win32 & set CONFIGURATION=Debug   & call build_curl.bat || goto :error
+set PLATFORM=Win32 & set CONFIGURATION=Release & call build_curl.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Debug   & call build_curl.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Release & call build_curl.bat || goto :error
 call bundle_curl.bat || goto :error
 
 goto :EOF
