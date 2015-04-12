1ed184fa866e6128fb43f60096297373f378a5a5
ethdev zug <marek.kotewicz@gmail.com>
2015-04-12 17:44:00 +0100

    fixed build on local machine

--- jsoncpp.bat
+++ jsoncpp.bat
@@ -1,13 +1,13 @@
 REM clone jsoncpp
 
-git clone -q https://github.com/debris/jsoncpp build\jsoncpp 
+if not exist build\jsoncpp git clone -q https://github.com/debris/jsoncpp build\jsoncpp 
 cd build\jsoncpp
 git checkout -qf 24c0054c10e62e8359c0f96372dfa183de90f93c
 
 REM create jsoncpp build dirs
-if %PLATFORM% == Win32 mkdir build
+if %PLATFORM% == Win32 if not exist build mkdir build
 if %PLATFORM% == Win32 cd build
-if %PLATFORM% == x64 mkdir buildx64
+if %PLATFORM% == x64 if not exist build64 mkdir buildx64
 if %PLATFORM% == x64 cd buildx64
 
 REM run jsoncpp cmake
@@ -16,4 +16,4 @@ if %PLATFORM% == x64 cmake -G "Visual Studio 12 2013 Win64" ..
 
 REM build jsoncpp
 %MSBuild% jsoncpp.sln /property:Configuration=%CONFIGURATION% /property:Platform=%PLATFORM%
-
+cd ..\..\..
--- main.bat
+++ main.bat
@@ -1,17 +1,19 @@
 REM add MSBuild to env variable
-set MSBuild:="\"c:/Program Files (x86)/MSBuild/12.0/bin/msbuild.exe\"";%MSBuild%
+set MSBuild="c:/Program Files (x86)/MSBuild/12.0/bin/msbuild.exe"
 
 REM create build directory
-mkdir build
+if not exist build mkdir build
 
-set PLATFORM=Win32 & set CONFIGURATION=Debug   & jsoncpp.bat || goto :error
-set PLATFORM=Win32 & set CONFIGURATION=Release & jsoncpp.bat || goto :error
-set PLATFORM=x64   & set CONFIGURATION=Debug   & jsoncpp.bat || goto :error
-set PLATFORM=x64   & set CONFIGURATION=Release & jsoncpp.bat || goto :error
+set PLATFORM=Win32 & set CONFIGURATION=Debug   & call jsoncpp.bat || goto :error
+set PLATFORM=Win32 & set CONFIGURATION=Release & call jsoncpp.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Debug   & call jsoncpp.bat || goto :error
+set PLATFORM=x64   & set CONFIGURATION=Release & call jsoncpp.bat || goto :error
 
-:goto :EOF
+goto :EOF
 
 :error
 echo Failed with error %errorlevel%
 exit /b %errorlevel%
+
+:EOF
 
