5e12fad8c24a3666049472bba1cc7c6e3a3f0bd2
ethdev zug <marek.kotewicz@gmail.com>
2015-04-13 21:47:25 +0100

    fixed bundling of cryptopp

--- bundle_cryptopp.bat
+++ bundle_cryptopp.bat
@@ -2,19 +2,20 @@ REM packaging cryptopp
 if not exist package\cryptopp mkdir package\cryptopp
 if not exist package\cryptopp\lib mkdir package\cryptopp\lib
 if not exist package\cryptopp\include mkdir package\cryptopp\include
+if not exist package\cryptopp\include\cryptopp mkdir package\cryptopp\include\cryptopp
 
 cd package\cryptopp
 
 if %PLATFORM% == Win32 (
-    if %CONFIGURATION% == Release cmake -E copy ..\..\build\cryptopp\win\Release\LibCryptopPP.lib lib\cryptopp.lib
-    if %CONFIGURATION% == Debug cmake -E copy ..\..\build\cryptopp\win\Debug\LibCryptopPP.lib lib\cryptoppd.lib
+    if %CONFIGURATION% == Release cmake -E copy ..\..\build\cryptopp\win\Release\LibCryptoPP.lib lib\cryptopp.lib
+    if %CONFIGURATION% == Debug cmake -E copy ..\..\build\cryptopp\win\Debug\LibCryptoPP.lib lib\cryptoppd.lib
 )
 
 if %PLATFORM% == x64 (
-    if %CONFIGURATION% == Release cmake -E copy ..\..\build\cryptopp\win\x64\Release\LibCryptopPP.lib lib\cryptopp_x64.lib
-    if %CONFIGURATION% == Debug cmake -E copy ..\..\build\cryptopp\win\x64\Debug\LibCryptopPP.lib lib\cryptoppd_x64.lib
+    if %CONFIGURATION% == Release cmake -E copy ..\..\build\cryptopp\win\x64\Release\LibCryptoPP.lib lib\cryptopp_x64.lib
+    if %CONFIGURATION% == Debug cmake -E copy ..\..\build\cryptopp\win\x64\Debug\LibCryptoPP.lib lib\cryptoppd_x64.lib
 )
 
-cmake -E copy_directory ..\..\build\cryptopp\include include
-cd ..\..
+xcopy ..\..\build\cryptopp\*.h include\cryptopp /sy
 
+cd ..\..
