b68499130730aa69a8d3443e33cede329baecb5e
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-13 13:20:43 +0200

    fixed typo

--- bundle_microhttpd.bat
+++ bundle_microhttpd.bat
@@ -50,7 +50,7 @@ REM zipping microhttpd
 cd package
 cmake -E rename microhttpd microhttpd-0.9.2
 tar -zcvf microhttpd-0.9.2.tar.gz microhttpd-0.9.2
-cd package ..
+cd ..
 
 REM installing microhttpd
 cmake -E copy_directory package\microhttpd-0.9.2 install
