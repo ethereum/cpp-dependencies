ad1603deb8666528e659d16d306d2530c2dec306
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-13 14:02:46 +0200

    common fixes

--- install_curl.bat
+++ install_curl.bat
@@ -3,6 +3,7 @@ cmake -E copy_directory package\curl install
 
 REM zipping curl
 cd package
+if exist curl-7.4.2 cmake -E remove_directory curl-7.4.2
 cmake -E rename curl curl-7.4.2
 tar -zcvf curl-7.4.2.tar.gz curl-7.4.2
 cd ..
--- install_jsoncpp.bat
+++ install_jsoncpp.bat
@@ -3,6 +3,7 @@ cmake -E copy_directory package\jsoncpp install
 
 REM zipping jsoncpp
 cd package
+if exist jsoncpp-1.6.2 cmake -E remove_directory jsoncpp-1.6.2
 cmake -E rename jsoncpp jsoncpp-1.6.2
 tar -zcvf jsoncpp-1.6.2.tar.gz jsoncpp-1.6.2
 cd ..
