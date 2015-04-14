05463f0633d6ecdd9a63fe9142e92748c59c183d
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-14 13:24:09 +0200

    fixed boost include path

--- bundle_boost.bat
+++ bundle_boost.bat
@@ -4,11 +4,11 @@ if not exist package\boost mkdir package\boost
 if not exist package\boost\%PLATFORM% mkdir package\boost\%PLATFORM%
 if not exist package\boost\%PLATFORM%\lib mkdir package\boost\%PLATFORM%\lib
 if not exist package\boost\%PLATFORM%\include mkdir package\boost\%PLATFORM%\include
-if not exist package\boost\%PLATFORM%\include\json mkdir package\boost\%PLATFORM%\include\json
+if not exist package\boost\%PLATFORM%\include\boost mkdir package\boost\%PLATFORM%\include\boost
 
 cd package\boost\%PLATFORM%
-    
-if %CONFIGURATION% == Release cmake -E copy_directory ..\..\..\build\boost\boost\boost include
+
+if %CONFIGURATION% == Release cmake -E copy_directory ..\..\..\build\boost\boost\boost include\boost
 if %CONFIGURATION% == Release cmake -E copy_directory ..\..\..\build\boost\boost\stage\%PLATFORM% lib
 
 cd ..\..\..
