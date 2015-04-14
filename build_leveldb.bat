74747e4b0aae1990a080ad40fac8fc51dac12c6f
ethdev zug <marek.kotewicz@gmail.com>
2015-04-14 15:31:09 +0100

    fixed leveldb

--- build_leveldb.bat
+++ build_leveldb.bat
@@ -1,7 +1,7 @@
 REM clone leveldb
 if not exist build\leveldb git clone -q https://github.com/debris/leveldb build\leveldb
 cd build\leveldb\win
-git checkout -qf 189e628b2d4024639927a7f94f87e13d3d9f5284
+git checkout -qf f778144af3b38149e8b86370cbf4509c62233902
 
 REM build leveldb
 %MSBuild% LibLevelDB.sln /property:Configuration=%CONFIGURATION% /property:Platform=%PLATFORM% /verbosity:minimal
