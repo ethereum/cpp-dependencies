eba879a2ae473c03e7b96afb9a5ba7c430069ef0
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-14 11:11:37 +0200

    new bundle structure: curl

--- bundle_cryptopp.bat
+++ bundle_cryptopp.bat
@@ -1,12 +1,12 @@
 REM packaging cryptopp
 if not exist package\cryptopp mkdir package\cryptopp
-if not exist package\cryptopp\Win32 mkdir package\cryptopp\Win32
-if not exist package\cryptopp\x64 mkdir package\cryptopp\x64
 
+if not exist package\cryptopp\Win32 mkdir package\cryptopp\Win32
 if not exist package\cryptopp\Win32\lib mkdir package\cryptopp\Win32\lib
 if not exist package\cryptopp\Win32\include mkdir package\cryptopp\Win32\include
 if not exist package\cryptopp\Win32\include\cryptopp mkdir package\cryptopp\Win32\include\cryptopp
 
+if not exist package\cryptopp\x64 mkdir package\cryptopp\x64
 if not exist package\cryptopp\x64\lib mkdir package\cryptopp\x64\lib
 if not exist package\cryptopp\x64\include mkdir package\cryptopp\x64\include
 if not exist package\cryptopp\x64\include\cryptopp mkdir package\cryptopp\x64\include\cryptopp
--- bundle_curl.bat
+++ bundle_curl.bat
@@ -1,48 +1,57 @@
 REM packaging curl
 if not exist package\curl mkdir package\curl
-if not exist package\curl\lib mkdir package\curl\lib
-if not exist package\curl\bin mkdir package\curl\bin
-if not exist package\curl\include mkdir package\curl\include
-if not exist package\curl\include\curl mkdir package\curl\include\curl
+
+if not exist package\curl\Win32 mkdir package\curl\Win32
+if not exist package\curl\Win32\lib mkdir package\curl\Win32\lib
+if not exist package\curl\Win32\bin mkdir package\curl\Win32\bin
+if not exist package\curl\Win32\include mkdir package\curl\Win32\include
+if not exist package\curl\Win32\include\curl mkdir package\curl\Win32\include\curl
+
+if not exist package\curl\x64 mkdir package\curl\x64
+if not exist package\curl\x64\lib mkdir package\curl\x64\lib
+if not exist package\curl\x64\bin mkdir package\curl\x64\bin
+if not exist package\curl\x64\include mkdir package\curl\x64\include
+if not exist package\curl\x64\include\curl mkdir package\curl\x64\include\curl
 
 cd package\curl
 
 if %PLATFORM% == Win32 (
     if %CONFIGURATION% == Release (
-        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl.dll bin\libcurl.dll
-        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl_imp.exp lib\libcurl.exp
-        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl_imp.lib lib\libcurl.lib
+        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl.dll Win32\bin\libcurl.dll
+        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl_imp.exp Win32\lib\libcurl.exp
+        cmake -E copy ..\..\build\curl\build\lib\Release\libcurl_imp.lib Win32\lib\libcurl.lib
     )
 
     if %CONFIGURATION% == Debug (
-        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.dll bin\libcurld.dll
-        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.ilk lib\libcurld.ilk
-        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.pdb lib\libcurld.pdb
-        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl_imp.exp lib\libcurld.exp
-        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl_imp.lib lib\libcurld.lib
+        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.dll Win32\bin\libcurld.dll
+        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.ilk Win32\lib\libcurld.ilk
+        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl.pdb Win32\lib\libcurld.pdb
+        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl_imp.exp Win32\lib\libcurld.exp
+        cmake -E copy ..\..\build\curl\build\lib\Debug\libcurl_imp.lib Win32\lib\libcurld.lib
     )
 
-    cmake -E copy ..\..\build\curl\build\include\curl\curlbuild.h include\curl\curlbuild.h
+    cmake -E copy_directory ..\..\build\curl\include\curl Win32\include\curl
+    cmake -E copy ..\..\build\curl\build\include\curl\curlbuild.h Win32\include\curl\curlbuild.h
 )
 
 if %PLATFORM% == x64 (
     if %CONFIGURATION% == Release (
-        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl.dll bin\libcurl_x64.dll
-        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl_imp.exp lib\libcurl_x64.exp
-        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl_imp.lib lib\libcurl_x64.lib
+        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl.dll x64\bin\libcurl.dll
+        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl_imp.exp x64\lib\libcurl.exp
+        cmake -E copy ..\..\build\curl\build64\lib\Release\libcurl_imp.lib x64\lib\libcurl.lib
     )
 
     if %CONFIGURATION% == Debug (
-        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.dll bin\libcurld_x64.dll
-        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.ilk lib\libcurld_x64.ilk
-        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.pdb lib\libcurld_x64.pdb
-        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl_imp.exp lib\libcurld_x64.exp
-        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl_imp.lib lib\libcurld_x64.lib
+        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.dll x64\bin\libcurld.dll
+        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.ilk x64\lib\libcurld.ilk
+        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl.pdb x64\lib\libcurld.pdb
+        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl_imp.exp x64\lib\libcurld.exp
+        cmake -E copy ..\..\build\curl\build64\lib\Debug\libcurl_imp.lib x64\lib\libcurld.lib
     )
 
-    cmake -E copy ..\..\build\curl\build64\include\curl\curlbuild.h include\curl\curlbuild.h
+    cmake -E copy_directory ..\..\build\curl\include\curl x64\include\curl
+    cmake -E copy ..\..\build\curl\build64\include\curl\curlbuild.h x64\include\curl\curlbuild.h
 )
 
-cmake -E copy_directory ..\..\build\curl\include\curl include\curl
 cd ..\..
 
