11dad838d99f80de91443ad085e970f428f56d21
debris <marek.kotewicz@gmail.com>
2015-07-29 21:10:05 +0200

    dont build Win32

--- main.bat
+++ main.bat
@@ -12,48 +12,33 @@ if not exist build mkdir build
 if not exist package mkdir package
 if not exist install mkdir install
 
-call :setup Win32 Debug   & call build_jsoncpp.bat     & call bundle_jsoncpp.bat || goto :error
-call :setup Win32 Release & call build_jsoncpp.bat     & call bundle_jsoncpp.bat || goto :error
 call :setup x64 Debug     & call build_jsoncpp.bat     & call bundle_jsoncpp.bat || goto :error
 call :setup x64 Release   & call build_jsoncpp.bat     & call bundle_jsoncpp.bat || goto :error
 call install_jsoncpp.bat || goto :error
 
-call :setup Win32 Debug   & call build_microhttpd.bat  & call bundle_microhttpd.bat || goto :error
-call :setup Win32 Release & call build_microhttpd.bat  & call bundle_microhttpd.bat || goto :error
 call :setup x64 Debug     & call build_microhttpd.bat  & call bundle_microhttpd.bat || goto :error
 call :setup x64 Release   & call build_microhttpd.bat  & call bundle_microhttpd.bat || goto :error
 call install_microhttpd.bat || goto :error
 
-call :setup Win32 Debug   & call build_curl.bat        & call bundle_curl.bat || goto :error
-call :setup Win32 Release & call build_curl.bat        & call bundle_curl.bat || goto :error
 call :setup x64 Debug     & call build_curl.bat        & call bundle_curl.bat || goto :error
 call :setup x64 Release   & call build_curl.bat        & call bundle_curl.bat || goto :error
 call install_curl.bat || goto :error
 
-call :setup Win32 Debug   & call build_jsonrpccpp.bat  & call bundle_jsonrpccpp.bat || goto :error
-call :setup Win32 Release & call build_jsonrpccpp.bat  & call bundle_jsonrpccpp.bat || goto :error
 call :setup x64 Debug     & call build_jsonrpccpp.bat  & call bundle_jsonrpccpp.bat || goto :error
 call :setup x64 Release   & call build_jsonrpccpp.bat  & call bundle_jsonrpccpp.bat || goto :error
 call install_jsonrpccpp.bat || goto :error
 
-call :setup Win32 Debug   & call build_cryptopp.bat    & call bundle_cryptopp.bat || goto :error
-call :setup Win32 Release & call build_cryptopp.bat    & call bundle_cryptopp.bat || goto :error
 call :setup x64 Debug     & call build_cryptopp.bat    & call bundle_cryptopp.bat || goto :error
 call :setup x64 Release   & call build_cryptopp.bat    & call bundle_cryptopp.bat || goto :error
 call install_cryptopp.bat || goto :error
 
-call :setup Win32 both    & call build_boost.bat       & call bundle_boost.bat || goto :error
 call :setup x64 both      & call build_boost.bat       & call bundle_boost.bat || goto :error
 call install_boost.bat
 
-call :setup Win32 Debug   & call build_leveldb.bat     & call bundle_leveldb.bat || goto :error
-call :setup Win32 Release & call build_leveldb.bat     & call bundle_leveldb.bat || goto :error
 call :setup x64 Debug     & call build_leveldb.bat     & call bundle_leveldb.bat || goto :error
 call :setup x64 Release   & call build_leveldb.bat     & call bundle_leveldb.bat || goto :error
 call install_leveldb.bat || goto :error
 
-call :setup Win32 Debug   & call build_miniupnpc.bat     & call bundle_miniupnpc.bat || goto :error
-call :setup Win32 Release & call build_miniupnpc.bat     & call bundle_miniupnpc.bat || goto :error
 call :setup x64 Debug     & call build_miniupnpc.bat     & call bundle_miniupnpc.bat || goto :error
 call :setup x64 Release   & call build_miniupnpc.bat     & call bundle_miniupnpc.bat || goto :error
 call install_miniupnpc.bat || goto :error
